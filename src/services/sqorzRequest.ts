102dde7008a2fdcdb749c436f92b7a374f58b85d
thibault <thibault.brun@gmail.com>
2024-04-20 23:06:37 +0200

    Add sqorzRequest for events with test

--- src/services/sqorzRequest.ts
+++ src/services/sqorzRequest.ts
@@ -1,6 +1,8 @@
 export class SqorzRequest {
 
-    public static ORGANIZATION_ENDPOINT: string = "https://our.sqorz.com/json/region/";
+    private static ORGANIZATION_ENDPOINT: string = "https://our.sqorz.com/json/region/";
+    private static EVENTS_ENDPOINT: string = "https://our.sqorz.com/json/events/";
+    private static EVENTS_BY_ORGANIZATION_ENDPOINT: string = "https://our.sqorz.com/json/org/";
 
     public static async getOrganizationsByCountry(countryCode: string) {
         const url = `${SqorzRequest.ORGANIZATION_ENDPOINT}${countryCode}`;
@@ -10,5 +12,30 @@ export class SqorzRequest {
         }
         const organizations = await response.json();
         return organizations;
+    }
+
+    public static async getEvents(regionCode: string | null, dateStart: string | null, dateEnd: string | null) {
+        const eventUrl = new URL(SqorzRequest.EVENTS_ENDPOINT);
+        if (regionCode) eventUrl.searchParams.append("regionCode", regionCode);
+        if (dateStart) eventUrl.searchParams.append("startDate", dateStart);
+        if (dateEnd) eventUrl.searchParams.append("endDate", dateEnd);
+
+        const response = await fetch(eventUrl.href);
+        if (!response.ok) {
+            throw new Error(`Error while fetching data from ${eventUrl.href}`);
+        }
+        const events = await response.json();
+        return events;
+    }
+
+    public static async getEventsByOrganization(organization: string) {
+        const url = `${SqorzRequest.EVENTS_BY_ORGANIZATION_ENDPOINT}${organization}`;
+        console.log(url);
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error while fetching data from ${url}`);
+        }
+        const events = await response.json();
+        return events;
     }
 }
--- src/test/services/sqorzRequest.test.js
+++ src/test/services/sqorzRequest.test.js
@@ -13,3 +13,28 @@ test('getOrganizationsByCountry with bad country code', async () =>  {
     .toThrow('Error while fetching data from');
 });
 
+
+// Events
+test('getEvents OK', async () => {
+    const events = await SqorzRequest.getEvents("fr","2024-04-17","2024-04-24");
+    expect(typeof events=='object').toBe(true);
+});
+
+
+test('getEvents with bad region code', async () =>  {        
+    await expect(SqorzRequest.getEvents("fromage","2024-04-17","2024-04-24"))
+    .rejects
+    .toThrow('Error while fetching data from');
+});
+
+test('getEventsByOrganization OK', async () => {
+    const events = await SqorzRequest.getEventsByOrganization("ffcnoaq");
+    expect(typeof events=='object').toBe(true);
+});
+
+
+test('getEventsByOrganization with bad region code', async () =>  {        
+    await expect(SqorzRequest.getEventsByOrganization("fromage"))
+    .rejects
+    .toThrow('Error while fetching data from');
+});
